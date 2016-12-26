a75af967a8e4abc9e9d2adff03d91a0b055f77e1
Gildas Quéméner <gildas.quemener@gmail.com>
2016-12-26 18:03:22 +0000

    Removed logic from sound driver

--- js/drivers/sound.js
+++ js/drivers/sound.js
@@ -1,41 +1,8 @@
-export default function sound(game$) {
-    let borders = [
-        new Audio('sounds/border_1.wav'),
-        new Audio('sounds/border_2.wav'),
-        new Audio('sounds/border_3.wav'),
-        new Audio('sounds/border_4.wav'),
-        new Audio('sounds/border_5.wav'),
-    ];
-    let shotLeft = new Audio('sounds/shot_left.wav');
-    let shotRight = new Audio('sounds/shot_right.wav');
-    let score = new Audio('sounds/score.wav');
-
-    const play = (audio) => {
-        audio.currentTime = 0;
-        audio.play();
-    }
-    const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
-
-    game$.addListener({ next: game => {
-        let paddles = game[0];
-        let ball = game[1];
-
-        if (null !== game.party.winner) {
-            play(score);
+export default function soundDriver(sound$) {
+    sound$.addListener({
+        next: sound => {
+            sound.currentTime = 0;
+            sound.play();
         }
-
-        if (game.ball.y >= 180 || game.ball.y < 0) {
-            let border = borders.splice(getRandomArbitrary(0, borders.length), 1)[0];
-            play(border);
-            borders.push(border);
-        }
-
-        if (75 === game.ball.x && game.ball.y >= game.paddles.left && game.ball.y <= game.paddles.left + 50) {
-            play(shotLeft);
-        }
-
-        if (305 === game.ball.x && game.ball.y >= game.paddles.right && game.ball.y <= game.paddles.right + 50) {
-            play(shotRight);
-        }
-    }});
+    });
 }
--- js/index.js
+++ js/index.js
@@ -1,34 +1,19 @@
 import xs from 'xstream';
 import {run} from '@cycle/xstream-run';
 import {makeDOMDriver, div, h2} from '@cycle/dom';
+import soundSink from './sinks/sound';
 import soundDriver from './drivers/sound';
+import getRandomArbitrary from './utils/random';
 
 function main(sources) {
-    const initialPosition = 70;
-    const paddleSpeed = 20;
-
-    const keys$ = sources.DOM.select('document').events('keydown');
-    const keyA$ = keys$.filter(ev => 'KeyA' === ev.code).mapTo(paddleSpeed);
-    const keyS$ = keys$.filter(ev => 'KeyS' === ev.code).mapTo(paddleSpeed * -1);
-    const keyJ$ = keys$.filter(ev => 'KeyJ' === ev.code).mapTo(paddleSpeed);
-    const keyK$ = keys$.filter(ev => 'KeyK' === ev.code).mapTo(paddleSpeed * -1);
-    const left$ = xs.merge(keyA$, keyS$).fold((x, y) => x + y, initialPosition);
-    const right$ = xs.merge(keyJ$, keyK$).fold((x, y) => x + y, initialPosition);
-    const paddles$ = xs.combine(left$, right$).map((combined) => {
-        return {
-            left: combined[0],
-            right: combined[1],
-        };
-    });
-
     const initParty = {
         party: {
             winner: null,
             ended: false,
         },
         paddles: {
-            left: 0,
-            right: 0,
+            left: 70,
+            right: 70,
         },
         ball: {
             x: 150,
@@ -37,6 +22,21 @@ function main(sources) {
             velY: 1
         }
     };
+
+    const keys$ = sources.DOM.select('document').events('keydown');
+    const keyA$ = keys$.filter(ev => 'KeyA' === ev.code).mapTo(1);
+    const keyS$ = keys$.filter(ev => 'KeyS' === ev.code).mapTo(-1);
+    const keyJ$ = keys$.filter(ev => 'KeyJ' === ev.code).mapTo(1);
+    const keyK$ = keys$.filter(ev => 'KeyK' === ev.code).mapTo(-1);
+    const left$ = xs.merge(keyA$, keyS$).fold((x, y) => x + y, initParty.paddles.left);
+    const right$ = xs.merge(keyJ$, keyK$).fold((x, y) => x + y, initParty.paddles.right);
+    const paddles$ = xs.combine(left$, right$).map((combined) => {
+        return {
+            left: combined[0],
+            right: combined[1],
+        };
+    });
+
     const game$ = xs.combine(paddles$, xs.periodic(15))
         .map((combine) => combine[0])
         .fold((game, paddles) => {
@@ -44,14 +44,15 @@ function main(sources) {
 
             if (null !== game.party.winner) {
                 game.party.ended = true;
+                return game;
             }
 
-            if (game.ball.x > 305) {
+            if (380 === game.ball.x) {
                 game.party.winner = 'player1';
                 return game;
             }
 
-            if (game.ball.x < 75) {
+            if (0 === game.ball.x) {
                 game.party.winner = 'player2';
                 return game;
             }
@@ -60,12 +61,14 @@ function main(sources) {
                 game.ball.velY = game.ball.velY * -1;
             }
 
-            if (75 === game.ball.x && game.ball.y >= paddles.left && game.ball.y <= paddles.left + 50) {
+            if (75 === game.ball.x && game.ball.y >= game.paddles.left && game.ball.y <= game.paddles.left + 50) {
                 game.ball.velX = game.ball.velX * -1;
+                game.ball.velY = game.ball.velY + getRandomArbitrary(-1, 2);
             }
 
-            if (305 === game.ball.x && game.ball.y >= paddles.right && game.ball.y <= paddles.right + 50) {
+            if (305 === game.ball.x && game.ball.y >= game.paddles.right && game.ball.y <= game.paddles.right + 50) {
                 game.ball.velX = game.ball.velX * -1;
+                game.ball.velY = game.ball.velY + getRandomArbitrary(-1, 2);
             }
 
             game.ball.x = game.ball.x + game.ball.velX;
@@ -90,7 +93,7 @@ function main(sources) {
                     div('#ball', { style: { top : game.ball.y + 'px', left: game.ball.x + 'px'} })
                 ]);
             }),
-        sound: game$
+        sound: soundSink(game$),
     };
 
     return sinks;
